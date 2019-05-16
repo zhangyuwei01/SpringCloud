a58d7743917b0326f27947cde73950cba67b7e79
ZhangYuWei <zhangyuwei01@foxmail.com>
2019-05-16 22:56:05 +0800

    修改B服务测试类的名字

--- spring-cloud-microservice-b/src/test/java/com/zyw/microservice/b/MicroserviceBApplicationTests.java
+++ spring-cloud-microservice-b/src/test/java/com/zyw/microservice/b/MicroserviceBApplicationTests.java
@@ -1,4 +1,4 @@
-package com.zyw.springcloudmicroserviceb;
+package com.zyw.microservice.b;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringCloudMicroserviceBApplicationTests {
+public class MicroserviceBApplicationTests {
 
 	@Test
 	public void contextLoads() {
