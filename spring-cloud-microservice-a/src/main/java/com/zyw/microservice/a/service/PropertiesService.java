bc6448739bd0964d1a701514de86c0b4b1298b2b
ZhangYuWei <zhangyuwei01@foxmail.com>
2019-05-20 21:47:48 +0800

    读取工程jar包外部的属性文件

--- spring-cloud-microservice-a/src/main/java/com/zyw/microservice/a/service/PropertiesService.java
+++ spring-cloud-microservice-a/src/main/java/com/zyw/microservice/a/service/PropertiesService.java
@@ -19,15 +19,23 @@ import com.zyw.microservice.a.util.RestUtils;
  */
 @Service
 @PropertySource(value = { "classpath:config/config1.properties", "classpath:config/config2.properties",
-		"file:${spring.properties.path}/config3.properties" }, ignoreResourceNotFound = true)
+		"file:${spring.properties.path}/config/config3.properties" }, ignoreResourceNotFound = true)
 public class PropertiesService {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	// 从配置文件中获取属性值
+	// 从工程配置文件中获取属性值
+	@Value("${spring.application.name}")
+	private String springApplicationName;
+	
+	// 从自定义配置文件中获取属性值
 	@Value("${property.one}")
 	private String propertyOne;
 	@Value("${property.two}")
 	private String propertyTwo;
+	
+	// 从工程外部配置文件中获取属性值
+//	@Value("${property.three}")
+//	private String propertyThree;
 
 	/**
 	 * @description 获取属性文件中的属性值
@@ -35,8 +43,10 @@ public class PropertiesService {
 	public RestResponse get() {
 		logger.info("进入get方法，无参数......");
 		Map<String, Object> propertiesMap = new HashMap<>();
+		propertiesMap.put("springApplicationName", springApplicationName);
 		propertiesMap.put("propertyOne", propertyOne);
 		propertiesMap.put("propertyTwo", propertyTwo);
+//		propertiesMap.put("propertyThree", propertyThree);
 		return RestUtils.returnSuccess(propertiesMap);
 	}
 
